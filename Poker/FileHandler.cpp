bc215b95bb00d1c0ab2a2035c3c8c1282f4b517b
brecht.derwael@gmail.com <brecht.derwael@gmail.com@f2c30c52-97e3-20b3-5ee6-175d7293cb46>
2012-07-09 13:50:24 +0000

    Jha tis naar de zak eh

--- Poker/FileHandler.cpp
+++ Poker/FileHandler.cpp
@@ -6,15 +6,17 @@
 
 using namespace std;
 
-void getFile (string& source, string& path)
-{
-	ifstream inFile;
-	string s;
-	inFile.open ( path );
-	while(!inFile.eof()) // To get you all the lines.
-    {
-		getline(inFile, s); // Saves the line in STRING.
-		source.append(s + "\n");
-    }
-	inFile.close();
+class FileHandler {
+	void getFile (string& source, string& path)
+	{
+		ifstream inFile;
+		string s;
+		inFile.open ( path );
+		while(!inFile.eof()) // To get you all the lines.
+		{
+			getline(inFile, s); // Saves the line in STRING.
+			source.append(s + "\n");
+		}
+		inFile.close();
+	}
 }
