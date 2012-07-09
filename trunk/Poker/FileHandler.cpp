fafa48732ddd2f03cc8683f04710ef432d00f6e1
brecht.derwael@gmail.com <brecht.derwael@gmail.com@f2c30c52-97e3-20b3-5ee6-175d7293cb46>
2012-07-09 11:31:41 +0000

    Uploaded all

---
+++ trunk/Poker/FileHandler.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "FileHandler.h"
+
+using namespace std;
+
+void getFile (string& source, string& path)
+{
+	ifstream inFile;
+	string s;
+	inFile.open ( path );
+	while(!inFile.eof()) // To get you all the lines.
+    {
+		getline(inFile, s); // Saves the line in STRING.
+		source.append(s + "\n");
+    }
+	inFile.close();
+}
